c1ef4c5db33e2b67da516633c7f594b28adaf47e
Enrico Fortunato <enrico.fortunato93@gmail.com>
2022-01-01 11:41:20 +0100

    cognito auth ready for prod test

--- src/frontend/js/main.js
+++ src/frontend/js/main.js
@@ -1,5 +1,5 @@
-// const loc = window.location.href.split('#')[0]
-const loc = "http://localhost:5501/src/frontend/index.html"
+const loc = window.location.href.split('#')[0]
+// const loc = "http://localhost:5501/src/frontend/index.html"
 const params = window.location.href.split('#')[1]
 
 console.log(loc);
@@ -7,12 +7,9 @@ console.log(params);
 
 
 if (typeof(params)==='undefined') {
-    const cognito_auth_uri = `https://hawkeye.auth.eu-west-1.amazoncognito.com/login?client_id=36irfpv253gapuun8e8u1c1g9r&response_type=token&scope=aws.cognito.signin.user.admin+email+openid+phone+profile&redirect_uri=`
+    const cognito_auth_uri = `https://hawkeye.auth.eu-west-1.amazoncognito.com/login?client_id=${_config.window.clientId}&response_type=token&scope=aws.cognito.signin.user.admin+email+openid+phone+profile&redirect_uri=`
     const relocation_uri = cognito_auth_uri + encodeURIComponent(loc)
     console.log(relocation_uri)
-    // const relocation_uri = "https://hawkeye.auth.eu-west-1.amazoncognito.com/login?client_id=36irfpv253gapuun8e8u1c1g9r&response_type=token&scope=aws.cognito.signin.user.admin+email+openid+phone+profile&redirect_uri=http%3A%2F%2Flocalhost%3A5501%2Fsrc%2Ffrontend%2Findex.html"
-    //                         https://hawkeye.auth.eu-west-1.amazoncognito.com/login?client_id=36irfpv253gapuun8e8u1c1g9r&response_type=token&scope=aws.cognito.signin.user.admin+email+openid+phone+profile&redirect_uri=http%3A%2F%2F127.0.0.1%3A5501%2Fsrc%2Ffrontend%2Findex.html
-    // const relocation_uri = "https://hawkeye.auth.eu-west-1.amazoncognito.com/login?client_id=36irfpv253gapuun8e8u1c1g9r&response_type=token&scope=aws.cognito.signin.user.admin+email+openid+phone+profile&redirect_uri=http%3A%2F%2Flocalhost%3A5500%2Fsrc%2Ffrontend%2Findex.html"
     window.location.replace(relocation_uri)
 }
 
@@ -30,34 +27,35 @@ const snap_img = document.querySelector('#snap-img');
 let header = new Headers();
 let snap_request = new Request('https://icpjhcglc3.execute-api.eu-west-1.amazonaws.com/prod/snap')
 
-// let snap_request = new Request('https://jsonplaceholder.typicode.com/posts')
 header.append('Authorization',accessParams.get("access_token"))
 const snap_init = {
     method: 'POST',
     headers: header,
-    // body: JSON.stringify({"test":"test"}),
     mode: 'no-cors',
     cache: 'default'
 }
 snap_btn.addEventListener('click', onClick);
 
-// snap_img.firstElementChild.src = images[counter]
-
 function onClick(e) {
-    e.preventDefault();
-    console.log(snap_btn);
+    e.preventDefault();  
     snap_btn.disabled = true
     snap_btn.textContent = 'refreshing...'
+    console.log(snap_btn);
 
     // Send post request
     fetch(snap_request,snap_init)
-    .then((res)=> res.json())
+    .then((res)=> {
+        console.log(res);
+        return res.json()})
     .then((data)=> console.log(data))
     .catch((err)=> console.log(err))
+    .then( () => {
+        // reactivate button
+        snap_btn.disabled = false
+        snap_btn.textContent = 'Refresh'
+    })
+
     // Listen for a reply with URL on a topic
 
-    // reactivate button
-    snap_btn.disabled = false
-    snap_btn.textContent = 'Refresh'
 }
 
