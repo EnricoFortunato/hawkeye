f811c9a7088c3680e940b2694d478edfd5c75c5d
Enrico Fortunato <enrico.fortunato93@gmail.com>
2022-01-02 15:34:53 +0100

    s3 frontend image added

--- src/frontend/js/main.js
+++ src/frontend/js/main.js
@@ -12,18 +12,77 @@ if (typeof(params)==='undefined') {
     window.location.replace(relocation_uri)
 }
 
-
 const accessParams = new URLSearchParams(params);
-
+const id_token = accessParams.get("id_token"); 
+const access_token = accessParams.get("access_token"); 
 console.log("id-token");
-console.log(accessParams.get("id_token"));
+console.log(id_token);
 console.log("access-token");
-console.log(accessParams.get("access_token"));
+console.log(access_token);
+
+const cognitoidentity = new AWS.CognitoIdentity({apiVersion: '2014-06-30',region:'eu-west-1'});
+
+const cognito_params = {
+    IdentityPoolId: 'eu-west-1:3db9b3fe-0e0b-4176-8217-7a06808dffb6', /* required */
+    AccountId: '081972465094',
+    Logins: {
+      'cognito-idp.eu-west-1.amazonaws.com/eu-west-1_1wzfZ5LC1': id_token,
+    }
+  };
+cognitoidentity.getId(cognito_params, (err, data) => {
+    if (err) console.log(err, err.stack); // an error occurred
+    else {
+        console.log(data);
+        identity_id = data.IdentityId
+
+        AWS.config.region = 'eu-west-1';
+        AWS.config.credentials = new AWS.CognitoIdentityCredentials({
+            IdentityPoolId: 'eu-west-1:3db9b3fe-0e0b-4176-8217-7a06808dffb6',
+            Logins: {
+                'cognito-idp.eu-west-1.amazonaws.com/eu-west-1_1wzfZ5LC1': id_token
+            }
+        });
+
+        AWS.config.credentials.get(function(err) {
+            if (err) return console.error(err);
+            else console.log(AWS.config.credentials);
+                                                                                                                                                                                                            
+            const s3 = new AWS.S3({
+                apiVersion: '2006-03-01',
+                params: {Bucket: 'hawkeye-data-storage'}
+            });
+
+            // s3.listObjects({Delimiter: '/'}, function(err, data) {
+            //     if (err) console.log(err, err.stack); // an error occurred
+            //     else {
+            //         console.log(data);
+
+            //     }
+            // });
+
+            s3.getObject({Key: 'frontend/refresh-image/refresh_image.jpg'}, function(err, data) {
+                if (err) console.log(err, err.stack); // an error occurred
+                else {
+                    console.log(data);
+                    snap_img.firstElementChild.src = "data:image/png;base64," + encode(data.Body);
+                }
+            });
+
+        });
+    }
+
+});
+
+function encode(data)
+{
+    var str = data.reduce(function(a,b){ return a+String.fromCharCode(b) },'');
+    return btoa(str).replace(/.{76}(?=.)/g,'$&\n');
+}
 
 const snap_btn = document.querySelector('#snap-button');
 const snap_img = document.querySelector('#snap-img');
 
-snap_img.firstElementChild.src = "https://hawkeye-data-storage.s3.eu-west-1.amazonaws.com/pi/snap/2021_12_23_20_35_41.jpg"
+//snap_img.firstElementChild.src = "https://hawkeye-data-storage.s3.eu-west-1.amazonaws.com/pi/snap/2021_12_23_20_35_41.jpg"
 
 let header = new Headers();
 let snap_request = new Request('https://icpjhcglc3.execute-api.eu-west-1.amazonaws.com/prod/snap')
