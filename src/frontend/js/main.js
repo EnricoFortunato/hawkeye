79f6868f9e680dab2c6cd4101795d41b105663aa
Enrico Fortunato <enrico.fortunato93@gmail.com>
2022-01-03 15:52:49 +0100

    resolved HWK-2

--- src/frontend/js/main.js
+++ src/frontend/js/main.js
@@ -1,24 +1,30 @@
+// Token validity
 const loc = window.location.href.split('#')[0].replace("127.0.0.1","localhost")
 const params = window.location.href.split('#')[1]
+let accessParams
+let id_token;
+let access_token;
 
-console.log(loc);
-console.log(params);
-
+const cognito_auth_uri = `https://hawkeye.auth.eu-west-1.amazoncognito.com/login?client_id=${_config.cognito.clientId}&response_type=token&scope=aws.cognito.signin.user.admin+email+openid+phone+profile&redirect_uri=`
+const relocation_uri = cognito_auth_uri + encodeURIComponent(loc)
 
 if (typeof(params)==='undefined') {
-    const cognito_auth_uri = `https://hawkeye.auth.eu-west-1.amazoncognito.com/login?client_id=${_config.cognito.clientId}&response_type=token&scope=aws.cognito.signin.user.admin+email+openid+phone+profile&redirect_uri=`
-    const relocation_uri = cognito_auth_uri + encodeURIComponent(loc)
-    console.log(relocation_uri)
     window.location.replace(relocation_uri)
 }
+else {
+    accessParams = new URLSearchParams(params);
+    id_token = accessParams.get("id_token"); 
+    access_token = accessParams.get("access_token");
 
-const accessParams = new URLSearchParams(params);
-const id_token = accessParams.get("id_token"); 
-const access_token = accessParams.get("access_token"); 
-console.log("id-token");
-console.log(id_token);
-console.log("access-token");
-console.log(access_token);
+    is_token_valid = true
+    if (!is_token_valid){
+        window.location.replace(relocation_uri)
+    }
+}
+
+// Authentication
+let s3;
+const key = 'frontend/refresh-image/refresh_image.jpg';
 
 const cognitoidentity = new AWS.CognitoIdentity({apiVersion: '2014-06-30',region:'eu-west-1'});
 
@@ -29,10 +35,10 @@ const cognito_params = {
       'cognito-idp.eu-west-1.amazonaws.com/eu-west-1_1wzfZ5LC1': id_token,
     }
   };
+
 cognitoidentity.getId(cognito_params, (err, data) => {
     if (err) console.log(err, err.stack); // an error occurred
     else {
-        console.log(data);
         identity_id = data.IdentityId
 
         AWS.config.region = 'eu-west-1';
@@ -43,51 +49,43 @@ cognitoidentity.getId(cognito_params, (err, data) => {
             }
         });
 
-        AWS.config.credentials.get(function(err) {
-            if (err) return console.error(err);
-            else console.log(AWS.config.credentials);
-                                                                                                                                                                                                            
-            const s3 = new AWS.S3({
-                apiVersion: '2006-03-01',
-                params: {Bucket: 'hawkeye-data-storage'}
-            });
-
-            // s3.listObjects({Delimiter: '/'}, function(err, data) {
-            //     if (err) console.log(err, err.stack); // an error occurred
-            //     else {
-            //         console.log(data);
-
-            //     }
-            // });
-
-            s3.getObject({Key: 'frontend/refresh-image/refresh_image.jpg'}, function(err, data) {
-                if (err) console.log(err, err.stack); // an error occurred
-                else {
-                    console.log(data);
-                    snap_img.firstElementChild.src = "data:image/png;base64," + encode(data.Body);
-                }
-            });
-
+        //first image refresh
+        s3 = new AWS.S3({
+            apiVersion: '2006-03-01',
+            params: {Bucket: 'hawkeye-data-storage'}
         });
+        
+        refresh_image(s3)
+        .then((res)=> console.log(res))
     }
-
 });
 
-function encode(data)
-{
+function refresh_image(s3) {
+    return new Promise((res,rej) => {
+        s3.getObject({Key: key}, (err, data) => {
+            if (err) {
+                console.log(err, err.stack); // an error occurred
+                rej(`Error: ${err}`)
+            }
+            else {
+                snap_img.firstElementChild.src = "data:image/png;base64," + encode(data.Body);
+                res(`Image updated`)
+            }
+        });
+    })
+}
+
+function encode(data) {
     var str = data.reduce(function(a,b){ return a+String.fromCharCode(b) },'');
     return btoa(str).replace(/.{76}(?=.)/g,'$&\n');
 }
 
 const snap_btn = document.querySelector('#snap-button');
 const snap_img = document.querySelector('#snap-img');
-
-//snap_img.firstElementChild.src = "https://hawkeye-data-storage.s3.eu-west-1.amazonaws.com/pi/snap/2021_12_23_20_35_41.jpg"
-
 let header = new Headers();
 let snap_request = new Request('https://icpjhcglc3.execute-api.eu-west-1.amazonaws.com/prod/snap')
 
-header.append('Authorization',accessParams.get("access_token"))
+header.append('Authorization',access_token)
 const snap_init = {
     method: 'POST',
     headers: header,
@@ -99,20 +97,24 @@ function onClick(e) {
     e.preventDefault();  
     snap_btn.disabled = true
     snap_btn.textContent = 'refreshing...'
-    console.log(snap_btn);
 
     // Send post request
     fetch(snap_request,snap_init)
     .then((res)=> {
         console.log(res);
         return res.json()})
-    .then((data)=> console.log(data))
+    .then((data)=> console.log("Snap request completed"))
     .catch((err)=> console.log(err))
     .then( () => {
-        // reactivate button
-        snap_btn.disabled = false
-        snap_btn.textContent = 'Refresh'
+        setTimeout(()=>{
+            // reactivate button
+            snap_btn.disabled = false
+            snap_btn.textContent = 'Refresh'
+            refresh_image(s3)
+        },1000)
     })
-
 }
 
+setInterval(() => {
+    refresh_image(s3);
+}, 1000);
